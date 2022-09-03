1b64381b14872445d2fb5c71d2f53efb17ecf850
Anupam Vamsi <anupam.ismav@gmail.com>
2022-09-03 14:58:25 +0530

    1. Add mode, entry and output options for basic webpack config, 2. Add html-webpack-plugin to generate dist/index.html through plugins (array) property

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+
+  entry: {
+    index: './src/index.js',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'A Restaurant Page',
+    }),
+  ],
+
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
