5f8a6970da09f09d5a1b38bc85d799ea3a907b2d
Anupam Vamsi <anupam.ismav@gmail.com>
2022-09-07 00:14:12 +0530

    1. Change heading on tab change

--- src/tabBook.js
+++ src/tabBook.js
@@ -11,6 +11,9 @@ export function loadContent() {
   para2.textContent +=
     "We allot three hours for all standard bookings! Of course, it is up to you to utilise the time you have, but we'd like to imagine you would break it down into an hour each of Ranting, Eating And Resting (REAR\u2122), to best utilise our soundproof rooms, our extremely tasty breakfast food and our beds with handpicked mattresses for the Best Rest You Can Ever Get\u2122!";
 
+  DOMElements.getElementOfSelector('.heading').textContent =
+    'Book The Experience';
+
   tabContent.appendChild(para1);
   tabContent.appendChild(para2);
 
